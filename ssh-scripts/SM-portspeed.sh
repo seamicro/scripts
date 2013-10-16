5903ed4d2f8dfcb783f238a691e7174e083bc849
amander <andrew.mander@amd.com>
2013-10-16 15:05:36 -0700

    Update SM-portspeed.sh
    Found differences between the Estoc and Sabre platforms in regards to Node Number sequence so made 2 separate array values. Still haven't put in the logic yet that detects which is which (primarily because I don't have an Opteron chassis to test with) but will add it in when the opportunity arises.

--- ssh-scripts/SM-portspeed.sh
+++ ssh-scripts/SM-portspeed.sh
@@ -20,6 +20,8 @@ SPEEDS="/tmp/speeds.out"
 FASTETHERNOIPG="0x00000199"
 FASTETHER="0x00140199"
 GIGETHER="0x00141000"
+ETH0SABRE="2 10 66 74 450 458 130 138 386 394 194 202 322 330 258 266 26 18 90 82 474 466 154 146 410 402 218 210 346 338 282 274 34 42 98 106 482 490 162 170 418 426 226 234 354 362 290 298 58 50 122 114 506 498 186 178 442 434 250 242 378 370 314 306"
+ETH0ESTOC="7 15 71 79 455 463 135 143 391 399 199 207 327 335 263 271 31 23 95 87 479 471 159 151 415 407 223 215 351 343 287 279 39 47 103 111 487 495 167 175 423 431 231 239 359 367 295 303 63 55 127 119 511 503 191 183 447 439 255 247 383 375 319 311"
 
 # SeaMicro Chassis Login - not sure why password is specified since it can't be passed via standard SSH.
 USER="admin"
@@ -30,6 +32,10 @@ PASS="seamicro"
 # Checks to see if SSH is up on the other end
 checkPort () {
 	$NC -vz $HOST 22 2>&1 > /dev/null	
+	if ! [[ $? = "0" ]]; then
+                echo -e "\nI tried connecting to: $HOST via SSH (port 22) but it failed. Please investigate.\n"
+                exit 1
+        fi
 }
 
 # Interface report - comes back with interface stats
@@ -37,6 +43,24 @@ intReport () {
 	echo -e "\n100Mbps Interfaces: `grep -vi "offset\|mask\|---" $SPEEDS | grep -i "$FASTETHERNOIPG\|$FASTETHER" | wc -l`"
 	echo -e "1000Mbps Interfaces: `grep -vi "offset\|mask\|---" $SPEEDS | grep -i "$GIGETHER" | wc -l`"
 	echo -e "Other: `grep -i FAB_ETH_TX_RATE_LIMIT $SPEEDS | grep -vi "offset\|mask\|---\|$FASTETHERNOIPG\|$FASTETHER\|$GIGETHER" | wc -l `\n"
+}
+
+# Get MAC addresses from the chassis
+getMAC () {
+	echo -e "\nGetting MAC address(es) from: $HOST"
+	ssh -l $USER $HOST "en ; show server summary $RANGE | include \"    0    \"" > $MACLIST
+        if [[ $? = "0" ]]; then
+		echo -e "\nMAC address(es) successfully obtained. File written to: $MACLIST"
+	else
+                echo -e "\nError occured during the MAC address acquisition process.\n"
+                exit 1
+        fi
+}
+
+# Converts the MAC addresses to the fabric endpoint
+baseEight () {
+	echo -e "\nConverting MAC addresses to fabric node IDs"
+	for i in `cat $MACLIST | cut -f2 -d- ` ; do OCTAL=`awk '{print $3}' $MACLIST| sed 's/^.//g;s/://g;s/....$*//g'` ; echo "ibase=8; $OCTAL" | bc ; done
 }
 
 # Checking to see if net cat is executable and warns then exits if it isn't
@@ -58,13 +82,31 @@ SHOWEDIT="$2"
 SERVER="$3"
 SPEED="$4"
 
+# Undocumented flag until properly implemented
+if [[ "$SHOWEDIT" = "getmac" ]]; then
+	if [[ "$SERVER" = "all" ]]; then
+		RANGE="all"
+		checkPort
+		getMAC
+		exit 0
+	elif ! [[ "$SERVER" -lt 0 || "$SERVER" -gt 63 ]]; then
+		RANGE=`echo $SERVER | sed "s/$/\/0/g"`
+		checkPort
+		getMAC
+		exi 0
+	else
+		echo -e "\nScope unknown. Please specify 'all' or a server ID from 0-63.\n"
+        	exit 1
+	fi
+fi
+
 # Show the current port speed
 if [[ "$SHOWEDIT" = "show" ]]; then
 	if [[ "$SERVER" = "all" ]]; then
 		SMOBID="0-511"
 	elif ! [[ "$SERVER" -lt 0 || "$SERVER" -gt 63 ]]; then
 		# The fabric mapping below for eth0. Starts at server 0 and goes up to server 63
-		set 2 10 66 74 450 458 130 138 386 394 194 202 322 330 258 266 26 18 90 82 474 466 154 146 410 402 218 210 346 338 282 274 34 42 98 106 482 490 162 170 418 426 226 234 354 362 290 298 58 50 122 114 506 498 186 178 442 434 250 242 378 370 314 306
+		set $ETH0ESTOC 
 		SERVEROFFSET=$(($SERVER+1))
 		SMOBID=`echo ${!SERVEROFFSET}`
 	else
@@ -76,13 +118,9 @@ if [[ "$SHOWEDIT" = "show" ]]; then
 
 # Checking to see if the remote host responds to an SSH connection
 	checkPort
-	if ! [[ $? = "0" ]]; then
-		echo -e "\nI tried connecting to: $HOST via SSH (port 22) but it failed. Please investigate.\n"
-		exit 1
-	fi
 
 # Logging into the chassis and displaying the port speed. Converting the hex values to something human readable
-	echo -e "\nConnecting to SERVER: $SERVER, Fabric Node ID: $SMOBID" | tee $SPEEDS
+	echo -e "\nConnecting to host: $SERVER, Fabric Node ID: $SMOBID" | tee $SPEEDS
 	ssh -l $USER $HOST "en ; unhide debug ; debug smob peek node $SMOBID regAddr 100a" >> $SPEEDS
 	grep -vi "Connecting\|offset\|mask\|---" $SPEEDS | sed "s/$GIGETHER/LINK RATE: 1000 Mbps/g;s/FASTETHERNOIPG/LINK RATE: 100 Mbps - Non-IPG/g;s/$FASTETHER/LINK RATE: 100 Mbps/g"
 	intReport
@@ -116,7 +154,7 @@ if [[ "$SERVER" = "all" ]]; then
 	SMOBID="0-511"
 elif ! [[ "$SERVER" -lt 0 || "$SERVER" -gt 63 ]]; then
 	# The fabric mapping below. 
-	set 2 10 66 74 450 458 130 138 386 394 194 202 322 330 258 266 26 18 90 82 474 466 154 146 410 402 218 210 346 338 282 274 34 42 98 106 482 490 162 170 418 426 226 234 354 362 290 298 58 50 122 114 506 498 186 178 442 434 250 242 378 370 314 306
+	set $ETH0ESTOC 
 	SERVEROFFSET=$(($SERVER+1))
 	SMOBID=`echo ${!SERVEROFFSET}`
 else	
@@ -125,7 +163,8 @@ else
 fi
 
 #Logging into the chassis to make the changes.
-echo -e "\nConnecting to SERVER: $SERVER, Fabric Node ID: $SMOBID" | tee $SPEEDS
+echo -e "\nConnecting to host: $SERVER, Fabric Node ID: $SMOBID" | tee $SPEEDS
 checkPort
-ssh -l $USER $HOST "en ; unhide debug ; debug smob poke node $SMOBID regAddr 100a value $USERMASK ; debug smob peek node $SMOBID regAddr 100a" > $SPEEDS
-grep -vi "offset\|mask\|---" $SPEEDS | sed "s/$GIGETHER/LINK RATE: 1000 Mbps/g;s/$FASTETHERNOIPG/LINK RATE: 100 Mbps - Non-IPG/g;s/$FASTETHER/LINK RATE: 100 Mbps/g"
+#ssh -l $USER $HOST "en ; unhide debug ; debug smob poke node $SMOBID regAddr 100a value $USERMASK ; debug smob peek node $SMOBID regAddr 100a" > $SPEEDS
+echo "ssh -l $USER $HOST \"en ; unhide debug ; debug smob poke node $SMOBID regAddr 100a value $USERMASK ; debug smob peek node $SMOBID regAddr 100a\" | tee  $SPEEDS"
+grep -vi "Connecting\|offset\|mask\|---" $SPEEDS | sed "s/$GIGETHER/LINK RATE: 1000 Mbps/g;s/$FASTETHERNOIPG/LINK RATE: 100 Mbps - Non-IPG/g;s/$FASTETHER/LINK RATE: 100 Mbps/g"
