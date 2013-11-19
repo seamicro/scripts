37e243e4fd6e8b6666fe5efcbdda21f51cd7e66a
vicenteg <vince.gonzalez@gmail.com>
2013-11-19 11:01:17 -0500

    reorganized code to be more easily read.

--- api-scripts/dhcp-generator/dhcp-generator.py
+++ api-scripts/dhcp-generator/dhcp-generator.py
@@ -5,20 +5,30 @@ import argparse
 import netaddr
 from seamicro_api import SeaMicroAPI
 
-HOSTNAME_FORMAT="server%d"
-IP_ADDRESS_FORMAT="192.168.1.%d"
-GATEWAY = "192.168.1.1"
 COBBLER_SYSTEM_TEMPLATE = "sudo cobbler system add --name %(name)s --mac %(serverMacAddr)s --ip-address %(ip_address)s --netmask %(netmask)s --gateway %(gateway)s --hostname %(name)s --dns-name %(name)s  --profile %(profile)s --interface eth0"
 ISC_DHCP_TEMPLATE = "host %(name)s { fixed-address %(ip_address)s; hardware ethernet %(serverMacAddr)s;}"
 
-def func(x,y):
-	"""Comparison function for use with builtin 'sorted'. Sorts the server serverId
-	   in numeric order."""
-	ccard_x = int(x['serverId'].split('/')[0])
-	ccard_y = int(y['serverId'].split('/')[0])
-	return ccard_x - ccard_y
 
-def argparser():
+def main():
+	args = parse_command_line_arguments()
+
+	api = SeaMicroAPI(hostname=args.chassis, verify_ssl=False)
+	api.login(username=args.username, password=args.password)
+	servers = api.servers_all()
+	api.logout()
+
+	server_nic_list = [ s for s in sorted(servers.values(), cmp=compare_server_id) if s['serverNIC'] == '0' ]
+
+	if args.cobbler:
+		template = COBBLER_SYSTEM_TEMPLATE
+	if args.dhcpd:
+		template = ISC_DHCP_TEMPLATE
+
+	for line in render_template(server_nic_list, args, template):
+		print line
+
+
+def parse_command_line_arguments():
 	parser = argparse.ArgumentParser(description='''\
 		Generate commands for cobbler and configuration lines for isc-dhcpd from SeaMicro MAC addresses.''')
 
@@ -66,6 +76,15 @@ def argparser():
 
 	return args
 
+
+def compare_server_id(x,y):
+	"""Comparison function for use with builtin 'sorted'. Sorts the server serverId
+	   in numeric order."""
+	ccard_x = int(x['serverId'].split('/')[0])
+	ccard_y = int(y['serverId'].split('/')[0])
+	return ccard_x - ccard_y
+
+
 def render_template(servers, args, template):
 	commands = [ ]
 	network = netaddr.IPNetwork('%s/%s' % (args.first_ip, args.netmask))
@@ -77,12 +96,12 @@ def render_template(servers, args, template):
 
 	for server in servers:
 		ccard = int(server['serverId'].split('/')[0])
-		hostname = HOSTNAME_FORMAT % ccard
+		hostname = args.base_name + str(ccard)
 		ip_address = args.first_ip + ccard
 		
 		for subnet in subnets:
 			if ip_address not in subnet:
-				print >>sys.stderr, "The number of servers do not fit in the range provided. Either change the first-ip option, or use a larger subnet mask."
+				print >>sys.stderr, "The number of servers found do not fit in the IP range provided. Either change the first-ip option, or use a larger subnet mask."
 				sys.exit(1)
 
 		server.update({ 'name': hostname, 'ip_address': ip_address, 'gateway': args.gateway, 'netmask': args.netmask })
@@ -92,23 +111,6 @@ def render_template(servers, args, template):
 		commands.append(template % server)
 	return commands
 
-def main():
-	args = argparser()
-
-	api = SeaMicroAPI(hostname=args.chassis, verify_ssl=False)
-	api.login(username=args.username, password=args.password)
-	servers = api.servers_all()
-	api.logout()
-
-	server_nic_list = [ s for s in sorted(servers.values(), cmp=func) if s['serverNIC'] == '0' ]
-
-	if args.cobbler:
-		template = COBBLER_SYSTEM_TEMPLATE
-	if args.dhcpd:
-		template = ISC_DHCP_TEMPLATE
-
-	for line in render_template(server_nic_list, args, template):
-		print line
 
 if __name__ == "__main__":
 	main()
